7fc747420a80788e67367fd14cc7acfbaaffeb64
Yidan Wang <wangyidan621@gmail.com>
2017-10-31 20:01:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,5 +2,5 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element){
 
-  return [element,... array]
+  return [element, ...array]
 }
